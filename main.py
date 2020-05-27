ae97cefbdd009e28c37a2a29896f88f6ff612a3d
Yuki-17 <62209480+Yuki-17@users.noreply.github.com>
2020-05-28 01:29:20 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -15,14 +15,17 @@ for i in range(data2):
 
 data3 = list(dict.fromkeys(datalist))
 
-data4 = {"rev_vol1":"","rev_vol2":"","rev_vol3":"","rev_vol4":""}
+
 
 data7 = {}
 for i in range(len(data3)):
 	data5 = {}
 	data6 = calendar.monthrange(int(data3[i][0:4]),int(data3[i][5:7]))[1]
-	for n in range(data6):
-		data5[n+1] = data4
+	for n in range(1,data6+1):
+		data4 = {"rev_vol1":"","rev_vol2":"","rev_vol3":"","rev_vol4":"","yyyymmdd":""}
+		data4.update(yyyymmdd=(data3[i]+"-"+str(n).zfill(2))) 
+		data5[n] = data4
+
 	data7[data3[i]] = data5
 
 
